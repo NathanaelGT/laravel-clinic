0218959e1d3cf43c1afda49e62ae8ca4cd98102a
NathanaelGT <gordontanu2004@gmail.com>
2021-02-05 10:29:58 +0800

    Fixing bug when adding a new schedule

--- resources/ts/admin/doctorList/addPlusMinusIconToSiblings.tsx
+++ resources/ts/admin/doctorList/addPlusMinusIconToSiblings.tsx
@@ -6,13 +6,13 @@ export default (elements: NodeListOf<Element>, applyLiveEdit: (_element: Element
     icon.onclick = () => {
       const siblingFirstChild = sibling.children[0]
       const siblingFirstChildText = (siblingFirstChild as HTMLElement).innerText.trim()
-      if (siblingFirstChildText === 'Tutup') siblingFirstChild.remove()
 
       if (icon.src === window.plusUrl) {
         const element = <span className="one-line text-success" />
         element.innerHTML = '<span class="editable" data-type="time" data-id="new">08:00 - 10:00</span> (per <span class="editable" data-type="per" data-id="new">30 menit</span>)'
         element.querySelectorAll('.editable').forEach(applyLiveEdit)
 
+        console.log(icon, sibling)
         const index = icon.previousElementSibling.childNodes.length - 1
         if (siblingFirstChildText !== 'Tutup') icon.previousElementSibling.childNodes[index].textContent = '), '
         icon.src = window.minusUrl
@@ -34,6 +34,7 @@ export default (elements: NodeListOf<Element>, applyLiveEdit: (_element: Element
           )
         }
       }
+      if (siblingFirstChildText === 'Tutup') siblingFirstChild.remove()
     }
 
     sibling.appendChild(icon)
